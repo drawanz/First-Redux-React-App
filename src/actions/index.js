53c080280f40487124ded94403e468c3ba1cb360
Leonidas <leonidas.drawanz@gmail.com>
2022-04-01 13:52:34 -0300

    actions criadas, thunk criado, falta implementar componente forms

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,28 @@
-// Coloque aqui suas actions
-
 export const ADD_EMAIL = 'ADD_EMAIL';
+export const GET_CURRENCIES = 'GET_CURRENCIES';
+export const GET_CURRENCIES_SUCESS = 'GET_CURRENCIES_SUCESS';
+export const GET_CURRENCIES_FAIL = 'GET_CURRENCIES_FAIL';
 
-const emailAction = (email) => ({ type: ADD_EMAIL, email });
+export const emailAction = (email) => ({ type: ADD_EMAIL, email });
 
-export default emailAction;
+export const actionGetCurrencies = () => ({ type: GET_CURRENCIES });
+
+export const actionGetCurrenciesSucess = (dataAPI) => ({
+  type: GET_CURRENCIES_SUCESS,
+  dataAPI,
+});
+
+export const actionGetCurrenciesFail = (error) => ({ type: GET_CURRENCIES_FAIL, error });
+
+export const thunkGetCurrencies = () => async (dispatch) => {
+  dispatch(actionGetCurrencies());
+  try {
+    const response = await fetch('https://economia.awesomeapi.com.br/json/all');
+    const data = await response.json();
+    const keys = Object.keys(data);
+    const dataAPI = delete keys.USDT;
+    dispatch(actionGetCurrenciesSucess(dataAPI));
+  } catch (e) {
+    dispatch(actionGetCurrenciesFail(e));
+  }
+};
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,19 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { GET_CURRENCIES, GET_CURRENCIES_SUCESS, GET_CURRENCIES_FAIL } from "../actions"
+
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+};
+
+const wallet = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case GET_CURRENCIES:
+    return { ...state };
+  case GET_CURRENCIES_SUCESS:
+    return { ...state, currencies: [...state.dataAPI] };
+  default:
+    return state;
+  }
+};
+
+export default wallet;
