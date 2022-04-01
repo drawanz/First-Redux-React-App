c376d54d4a775afc20900053682049de3a7a767a
Leonidas <leonidas.drawanz@gmail.com>
2022-04-01 11:36:28 -0300

    requisito 3

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
 
 export default class Wallet extends Component {
   render() {
     return (
-      <div>Wallet</div>
+      <div>
+        <Header />
+        Wallet
+      </div>
     );
   }
 }
